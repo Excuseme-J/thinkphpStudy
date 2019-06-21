d5de9d341637df4900f87875569aff565afc77f8
jilinkun <123456>
2019-06-21 09:49:11 +0800

    1day

---
+++ Application/Home/Controller/UserController.class.php
@@ -0,0 +1,16 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+/**
+* 
+*/
+class UserController extends Controller
+{
+	
+
+	//测试方法
+	public function test(){
+		echo $_GET['id'];
+	}
+}	
+?>
